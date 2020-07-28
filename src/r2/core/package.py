b6a5d793afbd0fa9359d7c4de4ca485b89adbe49
Nex Sabre <NexSabre@users.noreply.github.com>
2020-07-28 18:32:07 +0200

    Introduce a URL arguments

--- src/r2/core/package.py
+++ src/r2/core/package.py
@@ -1,7 +1,7 @@
 import json
 import logging
 import os
-from typing import AnyStr
+from typing import AnyStr, Dict
 
 from r2.install import Installation
 
@@ -21,36 +21,68 @@ class Package:
         return os.path.exists(abs_package_dir), abs_package_dir
 
     def save(self, endpoint, response_body) -> bool:
-        path, file = self._undress_endpoint(endpoint)
+        path, file, args = self._undress_endpoint(endpoint)
         self._ensure_path_exists(os.path.join(*(self._package_path, path)))
+        response_path = os.path.join(*(self._package_path, path, file))
 
-        with open(os.path.join(*(self._package_path, path, file)), 'w') as new_json:
+        outer_template = {
+            "data_id": os.path.join(path, file),
+            "actions": []
+        }
+
+        inner_template = {
+            "arguments": {},
+            "response": None
+        }
+
+        template = None
+        if os.path.isfile(response_path):
+            existing_file = self._load_existing_file(response_path)
+            template = existing_file if existing_file.get("data_id", False) else outer_template
+        else:
+            template = outer_template
+        known_arguments = [x for x in template["actions"] if args != x["arguments"]]
+
+        inner_template["arguments"], inner_template["response"] = args, response_body
+        known_arguments.append(inner_template)
+
+        template["actions"] = known_arguments
+
+        with open(response_path, 'w') as new_json:
             try:
-                if isinstance(response_body, dict):
-                    new_json.write(json.dumps(response_body))
+                if isinstance(template, dict):
+                    new_json.write(json.dumps(template))
                 else:
-                    new_json.write(response_body)
+                    new_json.write(template)
             except Exception as err:
                 print(err)
                 return False
             return True
 
+    def _load_existing_file(self, path):
+        existing_file = json.loads(self.__read(path))
+        if not isinstance(existing_file, dict):
+            return
+        return existing_file
+
     def load(self, endpoint):
-        path, file = self._undress_endpoint(endpoint)
+        path, file, args = self._undress_endpoint(endpoint)
         path_exist, path_absolute = self._ensure_path_exists(os.path.join(*(self._package_path, path)))
         if not os.path.exists(path_absolute):
             return None
         if not os.path.isfile(os.path.join(path_absolute, file)):
             return None
 
+        #  validate a arguments
         json_response = None
         raw_response = self.__read(os.path.join(path_absolute, file))
         try:
             json_response = json.loads(raw_response)
         except Exception as err:
             logging.error(f"Can not convert raw_response into json format. Is file corrupted?\n{err}")
-        finally:
-            return json_response
+
+        proper_response_base_on_args = [x for x in json_response["actions"] if args == x["arguments"]]
+        return proper_response_base_on_args[0]["response"] if proper_response_base_on_args else None
 
     @staticmethod
     def __read(location):
@@ -68,11 +100,24 @@ class Package:
         os.makedirs(os.path.join(self._package_path, path), exist_ok=True)
         return os.path.exists(path), path
 
-    @staticmethod
-    def _undress_endpoint(endpoint: str) -> (str, str):
+    def _undress_endpoint(self, endpoint: str) -> (AnyStr, AnyStr, Dict):
         l_endpoint = endpoint.split('/')
         if len(l_endpoint) > 1:
             path, filename = os.path.relpath(os.path.join(*l_endpoint[:-1])), l_endpoint[-1]
         else:
             path, filename = '', l_endpoint[-1]
-        return path, filename
+        args = self._make_args_dict_from_endpoint(endpoint)
+        if args:
+            filename = filename.split('?')[0]
+        return path, filename, args
+
+    @staticmethod
+    def _make_args_dict_from_endpoint(endpoint) -> Dict:
+        if '?' not in endpoint:
+            return {}
+        arguments = endpoint.split('?')[1:][0].split('&')
+        args: Dict = {}
+        for next_pair in arguments:
+            k, v = next_pair.split('=')
+            args[k] = v
+        return args
--- src/r2/core/test_package.py
+++ src/r2/core/test_package.py
@@ -19,8 +19,34 @@ class TestPackage(TestCase):
     def _save_example(self):
         return self.package.save("test/endpoint", {"test": "test_body"})
 
+    def test_save_arg_endpoint(self):
+        self.assertTrue(self._save_example_with_args())
+
+    def test_load_arg_endpoint(self):
+        self._save_example_with_args()
+        self.assertIsNotNone(self.package.load("test/endpoint?first=asdfgh&second=qwerty"))
+
+    def _save_example_with_args(self):
+        return self.package.save("test/endpoint?first=asdfgh&second=qwerty", {"test22": "test_body"})
+
+    def _save_example_with_args_diff(self):
+        pass
+
+    def test__make_args_dict_from_endpoint(self):
+        args = self.package._make_args_dict_from_endpoint("test/endpoint?first=asdfgh&second=qwerty")
+
+        self.assertIs(len(args), 2, "Should return 2 elements in the dictionary")
+        self.assertTrue(isinstance(args, dict), "Return should be a dictionary instance")
+
+    def test__undress_endpoint(self):
+        path, filename, args = self.package._undress_endpoint("test/endpoint?first=asdfgh&second=qwerty")
+        self.assertIsNotNone(path)
+        self.assertEqual(filename, "endpoint")
+        self.assertTrue(isinstance(args, dict))
+
     def tearDown(self) -> None:
         test_endpoint_location = os.path.join(*(Installation.DOWNLOAD_DIR, "default", "test", "endpoint"))
         if os.path.exists(test_endpoint_location):
             os.remove(test_endpoint_location)
+        if os.path.isdir(os.path.join(*(Installation.DOWNLOAD_DIR, "default", "test"))):
             os.rmdir(os.path.join(*(Installation.DOWNLOAD_DIR, "default", "test")))
