fe13adec0c60c21e08e70d804456088ffc4ad66e
Nex Sabre <NexSabre@users.noreply.github.com>
2020-07-28 18:42:52 +0200

    Test Package reformat

--- src/r2/core/test_package.py
+++ src/r2/core/test_package.py
@@ -4,42 +4,39 @@ from unittest import TestCase
 from r2.core.package import Package
 from r2.install import Installation
 
+ENDPOINT = "test/endpoint"
+ENDPOINT_W_ARG = "test/endpoint?first=asdfgh"
+ENDPOINT_W_ARGS = "test/endpoint?first=asdfgh&second=qwerty"
+
+TEST_RESPONSE = {"test": "test_body"}
+
 
 class TestPackage(TestCase):
     def setUp(self) -> None:
         self.package = Package()
 
     def test_save(self):
-        self.assertTrue(self._save_example())
+        self.assertTrue(self.package.save(ENDPOINT, TEST_RESPONSE))
 
     def test_load(self):
-        self._save_example()
-        self.assertIsNotNone(self.package.load("test/endpoint"))
-
-    def _save_example(self):
-        return self.package.save("test/endpoint", {"test": "test_body"})
+        self.package.save(ENDPOINT, TEST_RESPONSE)
+        self.assertIsNotNone(self.package.load(ENDPOINT))
 
     def test_save_arg_endpoint(self):
-        self.assertTrue(self._save_example_with_args())
+        self.assertTrue(self.package.save(ENDPOINT_W_ARGS, TEST_RESPONSE))
 
     def test_load_arg_endpoint(self):
-        self._save_example_with_args()
-        self.assertIsNotNone(self.package.load("test/endpoint?first=asdfgh&second=qwerty"))
-
-    def _save_example_with_args(self):
-        return self.package.save("test/endpoint?first=asdfgh&second=qwerty", {"test22": "test_body"})
-
-    def _save_example_with_args_diff(self):
-        pass
+        self.package.save(ENDPOINT_W_ARGS, TEST_RESPONSE)
+        self.assertIsNotNone(self.package.load(ENDPOINT_W_ARGS))
 
     def test__make_args_dict_from_endpoint(self):
-        args = self.package._make_args_dict_from_endpoint("test/endpoint?first=asdfgh&second=qwerty")
+        args = self.package._make_args_dict_from_endpoint(ENDPOINT_W_ARGS)
 
         self.assertIs(len(args), 2, "Should return 2 elements in the dictionary")
         self.assertTrue(isinstance(args, dict), "Return should be a dictionary instance")
 
     def test__undress_endpoint(self):
-        path, filename, args = self.package._undress_endpoint("test/endpoint?first=asdfgh&second=qwerty")
+        path, filename, args = self.package._undress_endpoint(ENDPOINT_W_ARGS)
         self.assertIsNotNone(path)
         self.assertEqual(filename, "endpoint")
         self.assertTrue(isinstance(args, dict))
