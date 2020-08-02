849b46ec7c45b15055138c31dec6b8a4c07267f9
Nex Sabre <NexSabre@users.noreply.github.com>
2020-08-02 11:08:06 +0200

    Resolve a issue with creation of the default directory at ~/.r2

--- src/r2/configuration.py
+++ src/r2/configuration.py
@@ -1,4 +1,5 @@
 import json
+import os
 from dataclasses import dataclass
 from os.path import join
 from typing import Dict
@@ -26,6 +27,8 @@ class Configuration:
 
     @staticmethod
     def write(config_dict: Dict):
+        if not os.path.exists(Installation.HOME_DIR):
+            os.mkdir(Installation.HOME_DIR)
         with open(Configuration.__config_path(), 'w') as config_file:
             config_file.write(json.dumps(config_dict))
 
