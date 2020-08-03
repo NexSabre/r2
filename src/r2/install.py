5f18adb95ef455d0ddd08b9ccca279839ed33cd7
Nex Sabre <NexSabre@users.noreply.github.com>
2020-08-03 18:30:49 +0200

    WIP: Better logging in for the terminal

--- src/r2/install.py
+++ src/r2/install.py
@@ -6,17 +6,18 @@ from os.path import expanduser, join, exists
 class Installation:
     HOME_DIR: str = join(expanduser('~'), '.r2')
     PACKAGES_DIR: str = join(HOME_DIR, "packages")
+    LOGS_DIR: str = join(HOME_DIR, "logs")
 
     def __init__(self):
         self._create_default_directories()
         self._verifies_directories()
 
     def _create_default_directories(self):
-        for d in (self.HOME_DIR, self.PACKAGES_DIR):
+        for d in (self.HOME_DIR, self.PACKAGES_DIR, self.LOGS_DIR):
             makedirs(d, exist_ok=True)
 
     def _verifies_directories(self):
-        for d in (self.HOME_DIR, self.PACKAGES_DIR):
+        for d in (self.HOME_DIR, self.PACKAGES_DIR, self.LOGS_DIR):
             if not exists(d):
                 logging.error(f"Can not create a {d} directory")
                 raise
@@ -28,6 +29,14 @@ class Installation:
             mkdir(Installation.HOME_DIR)
         except Exception:
             logging.error(f"Home directory cannot be created {Installation.HOME_DIR}")
+
+    @staticmethod
+    def create_logs_dir():
+        # noinspection PyBroadException
+        try:
+            mkdir(Installation.LOGS_DIR)
+        except Exception:
+            logging.error(f"Log directory cannot be created {Installation.LOGS_DIR}")
 
     @property
     def packages_path(self):
--- src/r2/main.py
+++ src/r2/main.py
@@ -1,11 +1,26 @@
+import logging
+
 from r2.actions.action_dispatcher import ActionsDispatcher
 from r2.helpers.constants import record_and_replay_ascii_logo
+from r2.install import Installation
 
 
 def main():
     print(record_and_replay_ascii_logo)
+
+    install()
+    start_logging()
+
     actions_dispatcher = ActionsDispatcher()
     actions_dispatcher.process_application()
+
+
+def start_logging():
+    logging.basicConfig(filename=f"{Installation.LOGS_DIR}/r2_custom")
+
+
+def install():
+    Installation()
 
 
 if __name__ == '__main__':
